206bec111db8c6e2c1b8ff3ce36d4c9f32162008
Michael Harper <mharper@imaware.health>
2022-03-01 07:07:37 -0700

    changing module declaration so i can use it

--- fhir-models-gen/main.go
+++ fhir-models-gen/main.go
@@ -1,4 +1,4 @@
-// Copyright 2019 - 2021 The Samply Community
+// Copyright 2019 - 2021 The mharper-imaware Community
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 package main
 
-import "github.com/samply/golang-fhir-models/fhir-models-gen/cmd"
+import "github.com/mharper-imaware/golang-fhir-models/fhir-models-gen/cmd"
 
 func main() {
 	cmd.Execute()
--- fhir-models/go.mod
+++ fhir-models/go.mod
@@ -1,3 +1,3 @@
 module github.com/mharper-imaware/golang-fhir-models/fhir-models
 
-go 1.17
+go 1.16
